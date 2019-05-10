586a26c407694e80c2cc27032d7d3f30939897b2
Lisa Ross <lisa@thisisbroad.com>
2019-05-10 11:39:26 -0400

    clock set up

---
+++ day2-clock/README.md
@@ -0,0 +1,15 @@
+# css + js clock
+
+Clock that takes in current time and updates the hands (second, minute, hour)
+
+rotate the hands depending on what time it is
+
+```css
+transform: rotate(20deg);
+```
+
+rotate on the very right hand side (in the middle)
+
+## TODO
+
+- better function to not have to recreate all the types(second,min,hour) since it's the same for each with a few exceptions
---
+++ day2-clock/src/index.js
@@ -0,0 +1,25 @@
+const secondHand = document.querySelector('.second-hand');
+const minutesHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+  // get the date right now
+  const now = new Date();
+  // get the second we are on now
+  const seconds = now.getSeconds();
+  const minutes = now.getMinutes();
+  const hour = now.getHours();
+  // get the degrees around the circle depending on the second we're at
+  const secondsDegrees = (seconds / 60) * 360 + 90;
+  const minutesDegrees = (minutes / 60) * 360 + 90;
+  const hourDegrees = (hour / 12) * 360 + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  minutesHand.style.transform = `rotate(${minutesDegrees}deg)`;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+  const time = hour + ':' + minutes + ':' + seconds;
+  console.log(time);
+}
+function runClock() {
+  // run set date every 1000ms (1sec)
+  setInterval(setDate, 1000);
+}
